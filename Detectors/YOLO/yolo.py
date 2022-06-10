8a19253b56de978339f9079fa36dfca7f07eb7b1
shahpratham <ppsprathamshah2313@gmail.com>
2022-06-11 00:47:34 +0530

    modified update_track

--- Detectors/YOLO/yolo.py
+++ Detectors/YOLO/yolo.py
@@ -1,41 +1,28 @@
-from pydoc import classname
-from turtle import color
-from unicodedata import name
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-# from utils import *
 from .darknet import darknet
 
-# cfg_file = "darknet/cfg/yolo.cfg"
-# weight_file = "darknet/yolov4.weights"
-# namesfile = "darknet/data/coco.names"
-# datafile = "darknet/cfg/coco.data"
-# m=""
-# class_names="darknet/data/coco.names"
-# colors=""
-
-# cfg_file = "Detectors/YOLO/darknet/cfg/yolo.cfg"
-# weight_file = "Detectors/YOLO/darknet/yolov4.weights"
-# namesfile = "Detectors/YOLO/darknet/data/coco.names"
-# datafile = "Detectors/YOLO/darknet/cfg/coco.data"
-# m=""
-# class_names="Detectors/YOLO/darknet/data/coco.names"
-# colors=""
-
-# cfg_file, datafile, namesfile, weight_file,m ,class_names,colors="","",'','','','',''
 class Yolo():
-    
+
+    cfg_file=" "
+    weight_file =" "
+    namesfile = ""
+    datafile = ""
+    m=""
+    class_names=""
+    colors=""
+
     def __init__(self, cfgfile, weightfile, name_file, data_file) -> None:
-        cfg_file, weight_file, namesfile ,datafile = cfgfile, weightfile, name_file, data_file
+        self.cfg_file,self.weight_file, self.namesfile ,self.datafile = cfgfile, weightfile, name_file, data_file
         self.load_model()
 
     def load_model(self):
         """
         Loads the YOLO darknet model
         """
-        m, class_names, colors = darknet.load_network(cfg_file, datafile, weight_file)
-        
+        self.m, self.class_names, self.colors = darknet.load_network(self.cfg_file, self.datafile, self.weight_file)
+    
     def detect(self, image):
         """
         Detects the person in the image.
@@ -46,9 +33,14 @@ class Yolo():
         Returns a list of detections where each item in list contains [x1, y1, x2, y2, conf]
         """
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # image = cv2.resize(image, (m.width, m.height))
-        # detect the objects
-        boxes = darknet.detect_image(m, class_names,image)
-        # plot the image with the bounding boxes and corresponding object class labels
-        darknet.draw_boxes(boxes, image, colors)
+        width = darknet.network_width(self.m)
+        height = darknet.network_height(self.m)
+        darknet_image = darknet.make_image(width, height, 3)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_rgb, (width, height),
+                                interpolation=cv2.INTER_LINEAR)
+        darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+        detections = darknet.detect_image(self.m, self.class_names, darknet_image)
+        res=darknet.draw_boxes(detections, image, self.colors)
+        # cv2.imshow("result",res)
+        return detections, res
--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from imutils.video import VideoStream
 import argparse
 import imutils
 import time
-
-# from Trackers.centroid_tracker.centroid import Centroid_tracker
-# from Detectors.detector import YOLO
+from Trackers.centroid_tracker.centroid import Centroid_tracker
 from Detectors.YOLO import yolo
 if __name__ == "__main__":
     # Add the arg parser
     
 
     # load the video
-    vs = VideoStream(src=0).start()
+    vs = VideoStream(src=0).start() 
     time.sleep(2.0)
-    # Initiate object detector
-    cfg_file = "Detectors/YOLO/darknet/cfg/yolo.cfg"
+
+    cfg_file = "Detectors/YOLO/darknet/cfg/yolov4.cfg"
     weight_file = "Detectors/YOLO/darknet/yolov4.weights"
     namesfile = "Detectors/YOLO/darknet/data/coco.names"
     datafile = "Detectors/YOLO/darknet/cfg/coco.data"
     m=""
     class_names="Detectors/YOLO/darknet/data/coco.names"
     colors=""
+
+    # Initiate object detector
     dect = yolo.Yolo(cfg_file, weight_file,namesfile, datafile)
     
     # Initiate tracker object
-    # ot = Centroid_tracker()
+    ot = Centroid_tracker()
     (H, W) = (None, None)
 
     # run the while loop
@@ -41,7 +41,11 @@ if __name__ == "__main__":
             (H, W) = frame.shape[:2]
 
         # run detection and get bbox
-        dect.detect(frame)
+        detections, res = dect.detect(frame)
+        cv2.imshow("result",res)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
         # run tracker update to get tracked tracks
-        # display the output
-        pass
+        ot.update(detections, ot.nextID)
