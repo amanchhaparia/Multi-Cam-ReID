95e78b053c9f28c1f98c81d4b9488e0e5cbf0e05
amanchhaparia <amanchhaparia125@gmail.com>
2022-11-09 12:21:42 +0530

    added fcdsc template

---
+++ Trackers/fcdsc_tracker/fcdsc_tracker.py
@@ -0,0 +1,32 @@
+import numpy as np
+from ..track import Track
+
+
+class fcdsc_tracker():
+    def __init__(self):
+        self.segments = []
+        self.tracklets = []
+        self.tracks = []
+
+    def feature_tracklet_association():
+        pass
+
+    def stitch_tracklets():
+        pass
+
+    def smooth_trajectory():
+        pass
+
+    def get_veclocity_cost_matrix():
+        pass
+
+    def get_deep_feat_cost_matrix():
+        pass
+
+    def apply_cdsc():
+        pass
+
+    def apply_fcdsc():
+        pass
+
+
